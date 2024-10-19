4a5b9c4a22e37e4707384477f3a712b90cc612ba
SonuDright <144533703+SonuDright@users.noreply.github.com>
2024-10-19 21:08:13 +0530

    i  added a authentication screen

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,11 @@ void main() async{
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:HomeScreen()
+      home:SplashScreen()
     );
   }
 }
