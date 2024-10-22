dbaf72d4dad0e221064233c756138137b11af938
SonuDright <144533703+SonuDright@users.noreply.github.com>
2024-10-22 14:41:57 +0530

    i changed in this app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_user_app/views/screens/home_screen.dart';
+import 'package:grocery_user_app/views/screens/splash_screen.dart';
 import 'firebase_options.dart';
 
 void main() async{
--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-import 'authantications/phone_auth.dart';
+import 'auth/signup_screen.dart';
+
 
 
 
